f34eb7a4be6f6f4428670492b1bd4c3a30e5956e
Raphaela Monteiro <raphaela.monteiro@waveconcept.com.br>
2026-04-02 18:00:12 -0300

    feat: adicionando arquivos para teste

--- WhatsApp.integration/Program.cs
+++ WhatsApp.integration/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var service = new WhatsAppService(
+    new HttpClient(),
+    config["WhatsApp:PhoneNumberId"]!,
+    config["WhatsApp:AccessToken"]!,
+    config["WhatsApp:ApiUrl"]!
+);
+
+await service.SendTemplateAsync(
+    to: "+55119XXXXXXXX",        // coloca o seu número aqui
+    templateName: "hello_world", // template que já vem aprovado em toda conta de teste
+    languageCode: "en_US",
+    variables: []
+);
